4a927e858687246e19e000a580a16badef4e450d
nunzio <chiefa.nunzio@gmail.com>
2021-06-02 17:54:44 +0200

    ex of relation

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'crudController@homeFun')
        ->name('home');   
        
-// Route::get('match/{id}', 'crudController@matchFun')
-//        ->name('match');   
-
-// Route::get('formMatch', 'crudController@formFun')
-//        ->name('formMatch');      
-
-// Route::post('addMatch', 'crudController@addMatch')
-//        ->name('addMatch');   
-
-// Route::get('editMatch/{id}', 'crudController@editFun')
-//        ->name('editMatch'); 
-       
-// Route::post('updateMatch/{id}', 'crudController@uptadeFun')
-//        ->name('updateMatch');    
-          
-// Route::get('delete/{id}','crudController@deleteMatch')   
-//        ->name('delete');  
+   
